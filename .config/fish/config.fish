573ff03a7d36e77b04148d95fb7bfb90c160fe7d
anonymous hacker <abhinavmgeorge@gmail.com>
2021-01-02 08:21:22 +0530

    adding .config folder

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+
+
+### RANDOM COLOR SCRIPT ###
+# Get this script from DerekTaylor's GitLab: gitlab.com/dwt1/shell-color-scripts
+# Or install it from the Arch User Repository: shell-color-scripts
+colorscript random
+
+alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+
+
+#starship prompt
+starship init fish | source
